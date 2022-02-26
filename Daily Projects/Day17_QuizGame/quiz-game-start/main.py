7649c3d3160f0c910ee2ee1cd0c51a38016f079d
Andrew Derbak <andrewderbak@gmail.com>
2022-02-26 07:32:34 -0600

    Finalized Day 17

--- Daily Projects/Day17_QuizGame/quiz-game-start/main.py
+++ Daily Projects/Day17_QuizGame/quiz-game-start/main.py
@@ -1,11 +1,11 @@
 from question_model import Question
-from data import question_data
+from data_api import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
 
 for i in question_data:
-    question_bank.append(Question(i['text'], i['answer']))
+    question_bank.append(Question(i['question'], i['correct_answer']))
 
 quiz = QuizBrain(question_bank)
 quiz.next_question()
