737e0f9946ed07f5e700449e6d0cd4994da60b1a
Andrew Derbak <andrewderbak@gmail.com>
2022-03-01 07:27:02 -0600

    Created starting file

---
+++ Daily Projects/Day018_TurtleGraphics/main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape('turtle')
+tim.color('cyan')
+
+# Challenge 1 - Draw a square
+#timmy_the_turtle.forward(100)
+for i in range(0,4):
+    tim.forward(100)
+    tim.left(90)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
