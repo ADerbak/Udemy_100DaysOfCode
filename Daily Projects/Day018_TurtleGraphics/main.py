1da10180c8ba129a1b235e45242c5b74f92879c7
Andrew Derbak <andrewderbak@gmail.com>
2022-03-01 07:37:26 -0600

    Completed challenges 1,2 for Day 18

--- Daily Projects/Day018_TurtleGraphics/main.py
+++ Daily Projects/Day018_TurtleGraphics/main.py
@@ -6,9 +6,16 @@ tim.color('cyan')
 
 # Challenge 1 - Draw a square
 #timmy_the_turtle.forward(100)
-for i in range(0,4):
-    tim.forward(100)
-    tim.left(90)
+# for i in range(0,4):
+#     tim.forward(100)
+#     tim.left(90)
+
+#Challenge 2 - Draw a dashed line
+for i in range(10):
+    tim.pendown()
+    tim.forward(5)
+    tim.penup()
+    tim.forward(5)
 
 
 
