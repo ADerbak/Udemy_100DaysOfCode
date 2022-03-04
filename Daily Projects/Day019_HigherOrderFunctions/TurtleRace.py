dbe9251feee85bdfac48ab69847c497f5658be49
Andrew Derbak <andrewderbak@gmail.com>
2022-03-04 09:26:37 -0600

    Finished Day 19

--- Daily Projects/Day019_HigherOrderFunctions/TurtleRace.py
+++ Daily Projects/Day019_HigherOrderFunctions/TurtleRace.py
@@ -1,15 +1,18 @@
+import turtle
 from turtle import Turtle, Screen
-
+import random
 
 screen = Screen()
 screen.setup(height=400, width=500)
 
 bet = screen.textinput(title="TURTLE RACE!", prompt = "Which turtle will win the race? Please enter a color: ")
-print(bet)
+
 
 colors = ['red', 'orange', 'gold', 'green', 'blue', 'purple']
 turtles = []
 increment = 0
+
+is_race_on = False
 
 for i in range(0,len(colors)):
     turtles.append(Turtle(shape='turtle'))
@@ -18,6 +21,28 @@ for i in range(0,len(colors)):
     turtles[i].goto(x = -230, y = -100 + increment)
     increment += 40
 
+if bet:
+    is_race_on = True
+
+winner = ''
+
+while is_race_on:
+    random_distance = random.randint(0,10)
+    turtles[random.randint(0,len(turtles)-1)].forward((random_distance))
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winner = str(turtle.color()[0])
+            if winner == bet:
+                print(f"Congratulations! The {winner} turtle won!")
+            else:
+                print(f"You lost! The {winner} turtle won the race.")
+
+
+
+
+screen.bye()
+#screen.exitonclick()
 
 
 
@@ -29,10 +54,3 @@ for i in range(0,len(colors)):
 
 
 
-
-
-
-
-
-
-screen.exitonclick()
