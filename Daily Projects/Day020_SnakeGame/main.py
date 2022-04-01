65731da491ece722aefbef903d1d3a3bceb29eb2
Andrew <andrewderbak@gmail.com>
2022-03-31 21:03:43 -0500

    Updates Snake class and main to use snake class

--- Daily Projects/Day020_SnakeGame/main.py
+++ Daily Projects/Day020_SnakeGame/main.py
@@ -1,69 +1,30 @@
 from turtle import Screen, Turtle
 import time
-import snake
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('--__--__-->~ Snake Game! ~<--__--__--')
+screen.tracer(0)
 
-snake_counter = 3
-snake_parts = []
-
-def move_forwards():
-    snake_parts[0].forward(20)
-
-def move_backwards():
-    snake_parts[0].backward(0)
-
-def rotate_right():
-    snake_parts[0].right(90)
-    #snake_parts[0].forward(20)
-
-def rotate_left():
-    snake_parts[0].left(90)
-    #snake_parts[0].forward(20)
+snake = Snake()
 
 
-
-for i in range(snake_counter):
-    snake_parts.append(Turtle('square'))
-    snake_parts[i].penup()
-    snake_parts[i].color('white')
-
-
-for i in range(0, len(snake_parts)):
-    if i == 0:
-        pass
-    else:
-        snake_parts[i].goto(snake_parts[i - 1].xcor() - 20, snake_parts[i - 1].ycor())
+# for i in range(0, len(snake_parts)):
+#     if i == 0:
+#         pass
+#     else:
+#         snake_parts[i].goto(snake_parts[i - 1].xcor() - 20, snake_parts[i - 1].ycor())
 
 game = True
 
-screen.tracer(0)
+
 
 while game:
     screen.update()
     time.sleep(0.1)
-    for i in range(len(snake_parts)-1, 0, -1):
-        if i > 0:
-            x_cor = snake_parts[i - 1].xcor()
-            y_cor = snake_parts[i - 1].ycor()
-            snake_parts[i].goto(x_cor, y_cor)
-        else:
-            pass
-    snake_parts[0].forward(20)
-
-
-    screen.listen()
-    screen.onkey(key="d", fun=rotate_right)
-    screen.onkey(key="a", fun=rotate_left)
-
-
-
-
-
-
+    snake.move()
 
 
 screen.exitonclick()
--- Daily Projects/Day020_SnakeGame/snake.py
+++ Daily Projects/Day020_SnakeGame/snake.py
@@ -1,15 +1,16 @@
 from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0),(-20, 0), (-40, 0)]
+MOVE_DISTNACE = 20
 
 class Snake:
     
-    def __init__(self_):
+    def __init__(self):
         self.segments = []
         self.create_snake()
         
     def create_snake(self):
-        for i in range(STARTING_POSITIONS):
+        for i in STARTING_POSITIONS:
             snake_part = Turtle('square')
             snake_part.penup()
             snake_part.color('white')
@@ -22,32 +23,16 @@ class Snake:
                 x_cor = self.segments[i - 1].xcor()
                 y_cor = self.segments[i - 1].ycor()
                 self.segments[i].goto(x_cor, y_cor)
-        self.segments[0].forward(20)
-
-
-def move_forwards():
-    snake_parts[0].forward(20)
-
-def move_backwards():
-    snake_parts[0].backward(0)
-
-def rotate_right():
-    snake_parts[0].right(90)
-    #snake_parts[0].forward(20)
-
-def rotate_left():
-    snake_parts[0].left(90)
-    #snake_parts[0].forward(20)
+        self.segments[0].forward(MOVE_DISTNACE)
 
 
 
 
 
 
-
-for i in range(0, len(snake_parts)):
-    if i == 0:
-        pass
-    else:
-        snake_parts[i].goto(snake_parts[i - 1].xcor() - 20, snake_parts[i - 1].ycor())
+# for i in range(0, len(snake_parts)):
+#     if i == 0:
+#         pass
+#     else:
+#         snake_parts[i].goto(snake_parts[i - 1].xcor() - 20, snake_parts[i - 1].ycor())
     
