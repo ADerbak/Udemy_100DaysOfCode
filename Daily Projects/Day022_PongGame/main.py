98a4c3c63632d0165d6781cf5d4ab465f3012561
Andrew <andrewderbak@gmail.com>
2022-08-04 17:06:05 -0500

    Updates screen and tries to create a paddle for Day22-Pong Game

--- Daily Projects/Day022_PongGame/main.py
+++ Daily Projects/Day022_PongGame/main.py
@@ -1,8 +1,18 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+
+
+
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+
+right_paddle = Turtle()
+right_paddle.shape("square")
+right_paddle.color("white")
+right_paddle.shapesize(stretch_len=100, stretch_wid=20)
+right_paddle.goto(x=350,y=0)
+right_paddle.onclick()
 
 screen.exitonclick()
---
+++ Daily Projects/Day022_PongGame/paddle.py
@@ -0,0 +1,5 @@
+from turtle import Turtle
+
+paddle = Turtle("square")
+paddle.xcor(350)
+paddle.ycor(0)
