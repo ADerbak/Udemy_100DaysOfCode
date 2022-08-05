03af39596ddbd7254c4dd3edacf1b473b47379f1
Andrew <andrewderbak@gmail.com>
2022-08-05 07:54:36 -0500

    Updates Paddle Class. Issues with up and down.

--- Daily Projects/Day022_PongGame/main.py
+++ Daily Projects/Day022_PongGame/main.py
@@ -1,5 +1,5 @@
 from turtle import Screen, Turtle
-
+from paddle import Paddle
 
 
 
@@ -7,35 +7,36 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
-screen.tracer(0)
+# screen.tracer(0)
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+# paddle = Turtle()
+# paddle.penup()
+# paddle.shape("square")
+# paddle.color("white")
+# paddle.shapesize(stretch_len=1, stretch_wid=5)
+# paddle.goto(x=350,y=0)
 
 
-paddle = Turtle()
-paddle.penup()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_len=1, stretch_wid=5)
-paddle.goto(x=350,y=0)
-
-
-def go_up():
-    new_y = paddle.ycor()+20
-    paddle.goto(paddle.xcor(), new_y)
+# def go_up():
+#     new_y = paddle.ycor()+20
+#     paddle.goto(paddle.xcor(), new_y)
     
-def go_down():
-    new_y = paddle.ycor()-20
-    paddle.goto(paddle.xcor(), new_y)
+# def go_down():
+#     new_y = paddle.ycor()-20
+#     paddle.goto(paddle.xcor(), new_y)
 
 
 
-    screen.listen()
-    screen.onkey(go_up, "Up")
-    screen.onkey(go_down, "Down")
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
 
-game_is_on = True
+# game_is_on = True
 
-while game_is_on:
-    screen.update()
+# while game_is_on:
+# screen.update()
 
 
-    screen.exitonclick()
+screen.exitonclick()
--- Daily Projects/Day022_PongGame/paddle.py
+++ Daily Projects/Day022_PongGame/paddle.py
@@ -1,5 +1,21 @@
 from turtle import Turtle
 
-paddle = Turtle("square")
-paddle.xcor(350)
-paddle.ycor(0)
+class Paddle:
+    
+    def __init__(self, coord):
+        x, y = coord
+        self = Turtle()
+        self.penup()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        
+        self.goto(x=x,y=y)
+        
+    def go_up(self):
+        new_y = self.ycor()+20
+        self.goto(self.xcor(), new_y)
+    
+    def go_down(self):
+        new_y = self.ycor()-20
+        self.goto(self.xcor(), new_y)
