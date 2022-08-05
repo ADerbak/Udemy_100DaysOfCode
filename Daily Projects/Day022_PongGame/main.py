f54ab10d1dc2ad9540b2408c910844c0ce2440ce
Andrew <andrewderbak@gmail.com>
2022-08-05 07:35:39 -0500

    Updates paddle and screen for Day 22

--- Daily Projects/Day022_PongGame/main.py
+++ Daily Projects/Day022_PongGame/main.py
@@ -7,12 +7,35 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 
-right_paddle = Turtle()
-right_paddle.shape("square")
-right_paddle.color("white")
-right_paddle.shapesize(stretch_len=100, stretch_wid=20)
-right_paddle.goto(x=350,y=0)
-right_paddle.onclick()
 
-screen.exitonclick()
+paddle = Turtle()
+paddle.penup()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_len=1, stretch_wid=5)
+paddle.goto(x=350,y=0)
+
+
+def go_up():
+    new_y = paddle.ycor()+20
+    paddle.goto(paddle.xcor(), new_y)
+    
+def go_down():
+    new_y = paddle.ycor()-20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+
+    screen.listen()
+    screen.onkey(go_up, "Up")
+    screen.onkey(go_down, "Down")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+
+
+    screen.exitonclick()
