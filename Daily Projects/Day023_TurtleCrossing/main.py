a72be5b6ef15957f4aaac676ff04915e2a7ad805
Andrew <andrewderbak@gmail.com>
2022-08-07 14:51:21 -0500

    Creates turtle player and moves/resets position.

--- Daily Projects/Day023_TurtleCrossing/main.py
+++ Daily Projects/Day023_TurtleCrossing/main.py
@@ -7,8 +7,19 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.bgcolor("white")
+
+player = Player()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    if player.ycor() == player.end:
+        player.reset_position()
+
+screen.exitonclick()
--- Daily Projects/Day023_TurtleCrossing/player.py
+++ Daily Projects/Day023_TurtleCrossing/player.py
@@ -1,7 +1,31 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+# Part 1 - Create player, make move from bottom to top of screen and reset.
+# Create a turtle player that starts at the bottom of the screen 
+# and listen for the "Up" keypress to move the turtle north. 
+# If you get stuck, check the video walkthrough in Step 3.
 
-class Player:
-    pass
+class Player(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.start = STARTING_POSITION
+        self.move = MOVE_DISTANCE
+        self.end = FINISH_LINE_Y
+        self.penup()
+        self.shape("turtle")
+        self.reset_position()
+        self.left(90)
+        
+        
+    def go_up(self):
+        new_y = self.ycor()+20
+        self.goto(self.xcor(), new_y)
+
+    def reset_position(self):
+        self.goto(self.start)
+        
