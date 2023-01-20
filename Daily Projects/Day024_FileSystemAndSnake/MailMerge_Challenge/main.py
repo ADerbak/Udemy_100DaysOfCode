3348c4df261200415766bc0d8e7a90e2e8ee5666
Andrew <andrewderbak@gmail.com>
2023-01-19 20:36:33 -0600

    Creates Day025_PandasGeoGame folder and initial challenge files

--- Daily Projects/Day024_FileSystemAndSnake/MailMerge_Challenge/main.py
+++ Daily Projects/Day024_FileSystemAndSnake/MailMerge_Challenge/main.py
@@ -4,18 +4,27 @@
 #Save the lterrsin the folder "ReadyToSend"
 
 guests = []
-with open('./Input/Names/invited_names.txt') as names:
+
+INVITEDGUESTS_FP = './Input/Names/invited_names.txt'
+LETTER_FP = './Input/Letters/starting_letter.txt'
+
+with open(INVITEDGUESTS_FP) as names:
     for line in names.readlines():
         guests.append(line.strip())
 
 # print(guests)  # for testing to make sure list comes through
 
 for name in guests:
-    with open('./Input/Letters/starting_letter.txt') as letter:
+    with open(LETTER_FP) as letter:
+        
+        # Clear out file if it already exists
         with open(f'./Output/ReadyToSend/{name}.txt', mode = 'w') as file:
-                    file.write('')
+                    file.write('') 
+                    
+        # For each line, append it on to the file.
         for line in letter.readlines():
             try: 
+                #If [name] is in the file, replace it with the name
                 if '[name]' in line:
                     line = line.replace('[name]',name)
             finally:
