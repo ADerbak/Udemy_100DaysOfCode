f693d37630a7d86bcbdf49dbc0cffb365a8f231c
Andrew <andrewderbak@gmail.com>
2023-01-21 13:33:46 -0600

    Fixes writer issue with game.

--- Daily Projects/Day025_PandasGeoGame/us-states-game-start/main.py
+++ Daily Projects/Day025_PandasGeoGame/us-states-game-start/main.py
@@ -24,22 +24,22 @@ answers = []
 
 game = True
 
-# while(game):
-writer.penup()
-# Get Answers from input
-answer_state = screen.textinput(title=f"{len(answers)}/50 Guess the state",prompt="What's another state's name?").title()
-print (answer_state in states) 
-if answer_state in states and answer_state not in answers:
-    answers.append(answer_state)
-    x,y = int(df[df.state == answer_state].x),int(df[df.state == answer_state].y)
-    print(x,y)
-
-    
-    writer.setposition(x,y)
-    writer.pendown()
-    turtle.write(answer_state, move=True)
+while(game):
     writer.penup()
-    # print(answer_state)
+    # Get Answers from input
+    answer_state = screen.textinput(title=f"{len(answers)}/50 Guess the state",prompt="What's another state's name?").title()
+    print (answer_state in states) 
+    if answer_state in states and answer_state not in answers:
+        answers.append(answer_state)
+        x,y = int(df[df.state == answer_state].x),int(df[df.state == answer_state].y)
+        print(x,y)
+
+        
+        writer.setposition(x,y)
+        writer.pendown()
+        writer.write(answer_state, move=True)
+        writer.penup()
+        print(answer_state)
 
 
 
