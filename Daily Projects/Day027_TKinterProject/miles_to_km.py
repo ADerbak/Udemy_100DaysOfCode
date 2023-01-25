4c6cff1065f36b4cc69ccea146c66ddbe6a90de1
Andrew <andrewderbak@gmail.com>
2023-01-24 19:22:40 -0600

    Finishes Day027

--- Daily Projects/Day027_TKinterProject/miles_to_km.py
+++ Daily Projects/Day027_TKinterProject/miles_to_km.py
@@ -3,34 +3,23 @@ from  tkinter import *
 window = Tk()
 
 window.title('Miles to KM converter!')
-window.minsize(width=300, height=200)
+window.config(padx = 20, pady=20)
 
-
-# Create a text entry
-def return_entry(en):
-    content = input.get()
-    print(content)
-
+# Entry
 input = Entry(window, width=10)
-
 input.grid(column=2,row=1)
-# name = ''
-# input.config(textvariable= name)
-# print(input.get())
-# Connect the entry with the return button
-input.bind('<Return>', return_entry)
 
-# Label
-is_equal_label = Label(text="is equal to", font = ('Ariel',24,'bold'))
+# Labels
+is_equal_label = Label(text="is equal to", font = ('Ariel'))
 is_equal_label.grid(column=1,row=2)
 
-km_amount_label = Label(text="0", font = ('Ariel',24,'bold'),)
+km_amount_label = Label(text="0", font = ('Ariel'),)
 km_amount_label.grid(column=2,row=2)
 
-miles_label = Label(text="Miles", font = ('Ariel',24,'bold'),)
+miles_label = Label(text="Miles", font = ('Ariel'),)
 miles_label.grid(column=3,row=1)
 
-km_label = Label(text="Km",font = ('Ariel',24,'bold'),)
+km_label = Label(text="Km",font = ('Ariel'),)
 km_label.grid(column=3,row=2)
 
 
@@ -38,14 +27,10 @@ km_label.grid(column=3,row=2)
 my_button = Button()
 
 def button_clicked():
-    km_amount_label.config(text=int(input.get()*1.6))
+    km_amount_label.config(text=str(int(input.get())*1.6))
 
 my_button.config(text = 'Calculate', command=button_clicked)
 my_button.grid(column=2,row=3)
-
-
-
-
 
 window.mainloop()
 
