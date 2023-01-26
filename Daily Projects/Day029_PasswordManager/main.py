8223f8901a6719fcc61b9a1861895bd8c811320b
Andrew <andrewderbak@gmail.com>
2023-01-25 21:08:28 -0600

    Creates Window, Logo, and mainloop
    Signed-off-by: Andrew <andrewderbak@gmail.com>

--- Daily Projects/Day029_PasswordManager/main.py
+++ Daily Projects/Day029_PasswordManager/main.py
@@ -1,5 +1,26 @@
+from tkinter import *
+import PIL.Image
+import PIL.ImageTk
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title('Password Manager')
+# window.minsize(400, 400)
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+# Fix for reading in image.
+image = PIL.Image.open('logo.png')
+logo = PIL.ImageTk.PhotoImage(image)
+
+canvas.create_image(100, 100, image=logo)
+canvas.pack()
+
+
+
+
+mainloop()
