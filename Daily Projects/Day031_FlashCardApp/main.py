79a6f88ec536992a17e2c93601150e01a7d63f12
Andrew <andrewderbak@gmail.com>
2023-02-18 18:34:55 -0600

    Creates card "flip" functionality.

--- Daily Projects/Day031_FlashCardApp/main.py
+++ Daily Projects/Day031_FlashCardApp/main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import random
 import json
 import pandas as pd
+import time
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -10,17 +11,34 @@ df = pd.read_csv('./data/french_words.csv')
 df = df.to_dict(orient="records")
 # french_word = df['French'][random.randint(0,len(df))]
 # word = df[random.randint(0,len(df))]['French']
-current_card = random.choice(df)
+current_card = {}
 language = "French"
 
 # ------------------- Yes/No Functions ----------------- #
 
 def next_card():
+    global current_card, timer
+    window.after_cancel(timer)
     # word = df[random.randint(0,len(df))]['French']
     current_card = random.choice(df)
     # window.update()
-    canvas.itemconfig(card_title, text= language)
-    canvas.itemconfigure(card_word, text=current_card[language])
+    canvas.itemconfig(card_image, image= card_front)
+    canvas.itemconfig(card_title, text= language,fill="black")
+    canvas.itemconfig(card_word, text=current_card[language], fill="black")
+    canvas.itemconfig(card_image, image= card_front)
+    timer = window.after(3000, func=back_of_card)
+    # check_button.after_cancel(timer)
+    # window.after_cancel(timer)
+
+    
+    
+def back_of_card():
+    # window.after(3000)    
+
+    canvas.itemconfig(card_image, image= card_back)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    timer
 
 
 
@@ -36,13 +54,14 @@ window = Tk()
 window.title('Flashy')
 window.minsize(width=900, height=700)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
+timer = window.after(3000, func=back_of_card)
 canvas = Canvas(width=800, height=526, highlightthickness=0, background=BACKGROUND_COLOR)
 # Fix for reading in image.
 # image = PIL.Image.open('logo.png')
 # logo = PIL.ImageTk.PhotoImage(image)
 card_front = PhotoImage(file='./images/card_front.png')
-canvas.create_image(400,275 , image=card_front)
+card_back = PhotoImage('./images/card_back.png')
+card_image = canvas.create_image(400,275, image = card_front)
 card_title = canvas.create_text((400,150), text="" ,  font= ("Ariel",40,'italic'), justify='center') #"italic"))#, __coords=(400,150))
 card_word = canvas.create_text((400,263),text="" ,  font= ("Ariel",60, 'bold'), justify='center') #60, "italic"))#, __coords=(400,263))
 
