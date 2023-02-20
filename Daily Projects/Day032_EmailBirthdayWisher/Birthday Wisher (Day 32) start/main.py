c96f907a826ec3e07d703a46a98716cbf8505a79
Andrew <andrewderbak@gmail.com>
2023-02-19 22:14:27 -0600

    Creates a Monday Motivation email.

--- Daily Projects/Day032_EmailBirthdayWisher/Birthday Wisher (Day 32) start/main.py
+++ Daily Projects/Day032_EmailBirthdayWisher/Birthday Wisher (Day 32) start/main.py
@@ -1,5 +1,9 @@
 import smtplib
 import yaml
+import datetime as dt
+import random
+
+
 with open('config.yml','r') as f:
     cfg = yaml.safe_load(f)
 
@@ -7,12 +11,18 @@ my_email = "derbaktest@gmail.com"
 password = cfg['email']['password'][1]
 test_email = "derbaktest@yahoo.com"
 
-with smtplib.SMTP("smtp.gmail.com") as connection: # create an SMTP object
-#smtp.mail.yahoo.com
-    connection.starttls() # Start secure encryption
-    connection.login(user=my_email, password= password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=test_email,
-        msg = "Subject:Hello!\n\nThis is the body of my email"
-    )
+if dt.datetime.now().weekday() == 6:
+    with open('quotes.txt', 'r') as f:
+        quotes = f.readlines()
+
+    quote = random.choice(quotes)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection: # create an SMTP object
+    #smtp.mail.yahoo.com
+        connection.starttls() # Start secure encryption
+        connection.login(user=my_email, password= password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=test_email,
+            msg = f"Subject:Your Monday Motivation!\n\n{quote}"
+        )
