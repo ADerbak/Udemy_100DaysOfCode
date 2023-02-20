dd2b75ac812ed7bf743ece82d73f50bd724905ce
Andrew <andrewderbak@gmail.com>
2023-02-19 22:40:58 -0600

    Successfully writes and sends an automated birthday message to email.

--- Daily Projects/Day032_EmailBirthdayWisher/Birthday Wisher (Day 32) start/main.py
+++ Daily Projects/Day032_EmailBirthdayWisher/Birthday Wisher (Day 32) start/main.py
@@ -1,4 +1,6 @@
 import smtplib
+
+import pandas as pd
 import yaml
 import datetime as dt
 import random
@@ -10,19 +12,27 @@ with open('config.yml','r') as f:
 my_email = "derbaktest@gmail.com"
 password = cfg['email']['password'][1]
 test_email = "derbaktest@yahoo.com"
+letters = ['letter_1.txt', 'letter_2.txt','letter_3.txt']
 
-if dt.datetime.now().weekday() == 6:
-    with open('quotes.txt', 'r') as f:
-        quotes = f.readlines()
 
-    quote = random.choice(quotes)
+df = pd.read_csv('birthdays.csv')
+birthdays = df.to_dict(orient='records')
 
-    with smtplib.SMTP("smtp.gmail.com") as connection: # create an SMTP object
-    #smtp.mail.yahoo.com
-        connection.starttls() # Start secure encryption
-        connection.login(user=my_email, password= password)
-        connection.sendmail(
-            from_addr=my_email,
-            to_addrs=test_email,
-            msg = f"Subject:Your Monday Motivation!\n\n{quote}"
-        )
+for row in birthdays:
+
+    if dt.datetime.now().month == row['month'] and dt.datetime.now().day == row['day']:
+        letter_template = random.choice(letters)
+        with open(letter_template, 'r') as f:
+             letter = f.read()
+             letter = letter.replace('[NAME]', row['name'])
+        # quote = random.choice(quotes)
+
+        with smtplib.SMTP("smtp.gmail.com") as connection: # create an SMTP object
+        #smtp.mail.yahoo.com
+            connection.starttls() # Start secure encryption
+            connection.login(user=my_email, password= password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=test_email,
+                msg = f"Subject:Happy Birthday!\n\n{letter}"
+            )
