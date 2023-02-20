b4b6ac72012a907ada99889758e2af62826f4002
Andrew <andrewderbak@gmail.com>
2023-02-19 21:59:35 -0600

    Cleans up code and auto-closes connection.

--- Daily Projects/Day032_EmailBirthdayWisher/Birthday Wisher (Day 32) start/main.py
+++ Daily Projects/Day032_EmailBirthdayWisher/Birthday Wisher (Day 32) start/main.py
@@ -7,10 +7,12 @@ my_email = "derbaktest@gmail.com"
 password = cfg['email']['password'][1]
 test_email = "derbaktest@yahoo.com"
 
-connection = smtplib.SMTP("smtp.gmail.com") # create an SMTP object
+with smtplib.SMTP("smtp.gmail.com") as connection: # create an SMTP object
 #smtp.mail.yahoo.com
-
-connection.starttls() # Start secure encryption
-connection.login(user=my_email, password= password)
-connection.sendmail(from_addr=my_email, to_addrs=test_email, msg = "Subject:Hello!\n\nThis is the body of my email")
-connection.close()
+    connection.starttls() # Start secure encryption
+    connection.login(user=my_email, password= password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=test_email,
+        msg = "Subject:Hello!\n\nThis is the body of my email"
+    )
