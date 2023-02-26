00880d5830bb57bde8d31e8704f24788b70ac38a
Andrew <andrewderbak@gmail.com>
2023-02-26 14:43:38 -0600

    Gets sunrise/sunset times for ISS station

--- Daily Projects/Day033_ISS_Tracker/main.py
+++ Daily Projects/Day033_ISS_Tracker/main.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 
 # Calling a basic API
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -11,9 +12,16 @@ if not response.raise_for_status():
     print(longitude, latitude)
 
 # Sending API with parameters
-response = requests.get(f"https://api.sunrise-sunset.org/json?lat={latitude}&lng={longitude}")
+response = requests.get(f"https://api.sunrise-sunset.org/json?lat={latitude}&lng={longitude}&formatted=0")
 if not response.raise_for_status():
     data = response.json()
-    sunrise = data['results']['sunrise']
-    sunset = data['results']['sunset']
+    sunrise = data['results']['sunrise'].split("T")[1].split(":")[0]
+    sunset = data['results']['sunset'].split("T")[1].split(":")[0]
     print(sunrise, sunset)
+
+time_now = datetime.datetime.now().hour
+
+print(time_now)
+
+
+
