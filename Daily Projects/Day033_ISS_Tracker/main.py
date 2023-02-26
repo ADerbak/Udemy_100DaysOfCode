c489832cde4a5e5f7a82b313e920426fa1bac804
Andrew <andrewderbak@gmail.com>
2023-02-26 14:37:11 -0600

    Adds on sunrise/sunset to ISS tracker. Cleans up code.

--- Daily Projects/Day033_ISS_Tracker/main.py
+++ Daily Projects/Day033_ISS_Tracker/main.py
@@ -1,11 +1,19 @@
 import requests
 
+# Calling a basic API
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 
+# Check for 200 status. If any issues arise, raise_for_status() will handle it
 if not response.raise_for_status():
     data = response.json()
-
     longitude = data['iss_position']['longitude']
     latitude = data['iss_position']['latitude']
+    print(longitude, latitude)
 
-
+# Sending API with parameters
+response = requests.get(f"https://api.sunrise-sunset.org/json?lat={latitude}&lng={longitude}")
+if not response.raise_for_status():
+    data = response.json()
+    sunrise = data['results']['sunrise']
+    sunset = data['results']['sunset']
+    print(sunrise, sunset)
