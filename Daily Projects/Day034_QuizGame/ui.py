7034061cba5d831a39d67e79695d7eb52d6c94f2
Andrew <andrewderbak@gmail.com>
2023-03-03 14:38:10 -0600

    Fixes ui.py

--- Daily Projects/Day034_QuizGame/ui.py
+++ Daily Projects/Day034_QuizGame/ui.py
@@ -9,44 +9,42 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(bg=THEME_COLOR)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         self.score = 0
         
         # ------------------- Score ----------------- #
         
-        self.text_box = Text(self.window
-                             , font= ("Ariel",12)
-                             , highlightcolor=THEME_COLOR
-                             , height = 12
-                             , width=30
-                             , background=THEME_COLOR
-                             ,highlightbackground=None)
-        self.text_box.grid(row=0, column=1)
-        self.text_box.insert('end',f"Score: {self.score}")
+        self.score_label = Label(text="Score: 0"
+                                 ,fg="white"
+                                 , bg=THEME_COLOR
+                                 ,highlightbackground=None)
+        self.score_label.grid(row=0, column=1)
         
         
         # ------------------- Canvas ----------------- #
         
-        self.canvas = Canvas(width=600
-                             , height=526
+        self.canvas = Canvas(width=300
+                             , height=250
                              , highlightthickness=0
-                             , bg=THEME_COLOR)
-        self.card_word = self.canvas.create_text((400,263)
-                                                 ,text="" 
-                                                 ,  font= ("Ariel",60, 'bold')
+                             , bg="white")
+        self.question_text = self.canvas.create_text( (150,125)
+                                                 ,text="Some Question Text"
+                                                 , fill=THEME_COLOR 
+                                                 ,  font= ("Ariel",20,'italic')
                                                  , justify='center')
 
-        self.canvas.grid(row=1, column=0, columnspan=2 )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50 )
+        
         
         # ------------------- Buttons ----------------- #
-        self.cross_image = PhotoImage(file = './images/false.png')
+        cross_image = PhotoImage(file = './images/false.png')
         self.cross_button = Button(image=self.cross_image
                             , highlightthickness=0
                             , pady=50
                             , bd=0)
         self.cross_button.grid(row=2, column=1)
 
-        self.check_image = PhotoImage(file = './images/true.png')
+        check_image = PhotoImage(file = './images/true.png')
         self.check_button = Button( image=self.check_image
                             , highlightthickness=0
                             , pady=50
