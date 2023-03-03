1ae255d2d5d144669af0a7b1fd56e4c613fe444c
Andrew <andrewderbak@gmail.com>
2023-03-03 14:08:31 -0600

    Creates quiz game ui class and imports data

--- Daily Projects/Day034_QuizGame/data.py
+++ Daily Projects/Day034_QuizGame/data.py
@@ -9,7 +9,7 @@ response = requests.get("https://opentdb.com/api.php", params=params)
 response.raise_for_status()
 data = response.json()
 question_data = data['results']
-print(question_data)
+# print(question_data)
 # question_data = [
 #     {
 #         "category": "Science: Computers",
--- Daily Projects/Day034_QuizGame/ui.py
+++ Daily Projects/Day034_QuizGame/ui.py
@@ -1,2 +1,17 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+
+class QuizInterface:
+    
+    def __init__(self):
+        self.window = TK()
+        self.windiow.title("Quizzler")
+        
+        
+        
+        
+        
+        self.window.mainloop()
