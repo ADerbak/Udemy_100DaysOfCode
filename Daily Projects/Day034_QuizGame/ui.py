e869c96602bcaff32ee1c3fd3b889b3c6ab2f6df
Andrew <andrewderbak@gmail.com>
2023-03-03 15:04:09 -0600

    Fixes ui. Populates question text.

--- Daily Projects/Day034_QuizGame/ui.py
+++ Daily Projects/Day034_QuizGame/ui.py
@@ -1,4 +1,6 @@
-from tkinter import *
+from tkinter import Tk, Label, Button, Canvas, PhotoImage
+from quiz_brain import QuizBrain
+
 
 THEME_COLOR = "#375362"
 
@@ -6,7 +8,9 @@ THEME_COLOR = "#375362"
 
 class QuizInterface:
     
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -31,39 +35,36 @@ class QuizInterface:
                                                  ,text="Some Question Text"
                                                  , fill=THEME_COLOR 
                                                  ,  font= ("Ariel",20,'italic')
-                                                 , justify='center')
+                                                 , justify='center'
+                                                 , width=280)
 
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50 )
         
         
         # ------------------- Buttons ----------------- #
         cross_image = PhotoImage(file = './images/false.png')
-        self.cross_button = Button(image=self.cross_image
+        self.cross_button = Button(image=cross_image
                             , highlightthickness=0
                             , pady=50
                             , bd=0)
         self.cross_button.grid(row=2, column=1)
 
         check_image = PhotoImage(file = './images/true.png')
-        self.check_button = Button( image=self.check_image
+        self.check_button = Button( image=check_image
                             , highlightthickness=0
                             , pady=50
                             , bd=0)
                             
         self.check_button.grid(row=2, column=0)
         
+        self.get_next_question()
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
     
         self.window.mainloop()
+        
+        
+        
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+        
