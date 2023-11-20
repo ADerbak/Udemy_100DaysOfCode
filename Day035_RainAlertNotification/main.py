cd1e68382e6426a95865455c91a5b39d912d247f
Andrew <andrewderbak@gmail.com>
2023-11-20 14:00:18 -0600

    Cleans up api call. Raises error if not 200. excludes calls we do not need. Changes API call due to subscription changes.

--- Day035_RainAlertNotification/main.py
+++ Day035_RainAlertNotification/main.py
@@ -2,12 +2,13 @@ import requests as requests
 
 from secrets import app_id, lat, long
 
-api = f"https://api.openweathermap.org/data/3.0/onecall"
+# api = f"https://api.openweathermap.org/data/3.0/onecall"
+api = f"https://api.openweathermap.org/data/2.5/weather"
 weather_params = {
     "lat": lat,
-    "long": long,
-    "app_id": app_id,
-    "exclude": "daily,current,minutely"
+    "lon": long,
+    "appid": app_id,
+    # "exclude": "daily,current,minutely"
 
 }
 
@@ -15,5 +16,14 @@ response = requests.get(api, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 print(weather_data)
+
+# find if <7xx code called in hourly.weather.id for the next 12 id's
+for i in range(0, 12):
+    hourly_weather=weather_data.hourly[i]
+    print(hourly_weather.weather.id)
+    if hourly_weather.weather[0].id < 700:
+        print("Bring an umbrella! Bad weather in next 12 hours")
+        break
+
 
 
