441266fd839ed76723ecfd01c772b470887ddabc
Andrew <andrewderbak@gmail.com>
2023-11-20 14:09:49 -0600

    Changes logic to get only today's forecast due to subscription changes.

--- Day035_RainAlertNotification/main.py
+++ Day035_RainAlertNotification/main.py
@@ -15,15 +15,18 @@ weather_params = {
 response = requests.get(api, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-print(weather_data)
+# print(weather_data)
+# print(weather_data["weather"][0]["id"])
 
 # find if <7xx code called in hourly.weather.id for the next 12 id's
-for i in range(0, 12):
-    hourly_weather=weather_data.hourly[i]
-    print(hourly_weather.weather.id)
-    if hourly_weather.weather[0].id < 700:
-        print("Bring an umbrella! Bad weather in next 12 hours")
-        break
+# for i in range(0, 12):
+#     hourly_weather = weather_data.hourly[i]
+#     print(hourly_weather.weather.id)
+#     if int(hourly_weather.weather[0].id[0]) < 7:
+#         print("Bring an umbrella! Bad weather in next 12 hours")
+#         break
 
-
+# Creating the same for the available API call
+if int(weather_data["weather"][0]["id"]) < 700:
+    print("Bring an umbrella!")
 
