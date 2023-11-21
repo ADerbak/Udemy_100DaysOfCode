91bf43a50596dd235f2a5836c7e02e7e9c502458
Andrew <andrewderbak@gmail.com>
2023-11-21 10:13:02 -0600

    Calls TSLA stock with API.

--- Daily Projects/Day036_StockNewsTracker/main.py
+++ Daily Projects/Day036_StockNewsTracker/main.py
@@ -1,8 +1,33 @@
+import os
+
+import requests as requests
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+api = "https://www.alphavantage.co/query"
+# ?function=TIME_SERIES_DAILY&symbol={{SYMBOL}}&outputsize=compact&apikey={{ALPHA_VANTAGE_KEY}}
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "outputsize":"compact",
+    "apikey": os.environ.get('ALPHA_VANTAGE_API_KEY')
+}
+
+results = requests.get(api, params=stock_params)
+
+results_json = results.json()
+yesterday_date = next(iter(results_json["Time Series (Daily)"]))
+last_price = results_json["Time Series (Daily)"][yesterday_date]
+price_diff = float(abs(float(last_price["4. close"])/float(last_price["1. open"])-1))
+
+if price_diff > 0.5:
+    print("Get News!")
+else:
+    print("{:.2f}".format(price_diff) + " change, no news")
+
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
