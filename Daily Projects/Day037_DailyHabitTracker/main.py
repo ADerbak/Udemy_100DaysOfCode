ee812dc30a531d75b3d3b383afb1b851474328e6
Andrew <andrewderbak@gmail.com>
2023-11-26 17:09:06 -0600

    Successfully updates today's value for pages read.

--- Daily Projects/Day037_DailyHabitTracker/main.py
+++ Daily Projects/Day037_DailyHabitTracker/main.py
@@ -15,6 +15,8 @@ user_params = {
 # print(response.text)
 # {"message":"Success. Let's visit https://pixe.la/@aderbak , it is your profile page!","isSuccess":true}
 
+
+# Create a new graph
 pixela_graph_endpoint = f"{pixela_endpoint}/{secrets.pixela_username}/graphs"
 
 graph_config = {
@@ -27,6 +29,20 @@ graph_config = {
 
 pixela_header = {"X-USER-TOKEN": secrets.pixela_token}
 
-response = requests.post(pixela_graph_endpoint, json=graph_config, headers=pixela_header)
-print(response.text)
+# response = requests.post(pixela_graph_endpoint, json=graph_config, headers=pixela_header)
+# print(response.text)
 # {"message":"Success.","isSuccess":true}
+
+# Post a value to the graph
+pixela_value_endpoint = f"{pixela_graph_endpoint}/reading"
+
+value_config = {
+    "date": "20231126",
+    "quantity": "40",
+    "optionalData": {"Books": "Moonwalking with Einstein; A Very Punchable face"}
+}
+
+print(pixela_value_endpoint)
+
+response = requests.post(pixela_value_endpoint, json=value_config, headers=pixela_header)
+print(response)
