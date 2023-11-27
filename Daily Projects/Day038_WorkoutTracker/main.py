797b4d53672273f0c8450cbe332bdb9361421876
Andrew <andrewderbak@gmail.com>
2023-11-26 21:03:21 -0600

    Completes Day 038 challenge. All code (and secrets) transferred to REPLit as needed.

--- Daily Projects/Day038_WorkoutTracker/main.py
+++ Daily Projects/Day038_WorkoutTracker/main.py
@@ -1,7 +1,13 @@
+import datetime
+import json
+
 import requests
 import os
 
 import user_metrics
+
+
+# Nutritionix Exercise Stats
 
 app_id = os.environ.get('NUTRIONIX_APP_ID')
 app_key = os.environ.get('NUTRIONIX_APP_KEY')
@@ -25,4 +31,31 @@ query_params = {
 }
 
 response = requests.post(uri, json=query_params, headers=app_headers)
-print(response.text)
+
+if not response.raise_for_status():
+    response_dict = json.loads(response.text)
+    for workout in response_dict["exercises"]:
+        exercise = str(workout["name"]).title()
+        duration = workout["duration_min"]
+        calories = workout["nf_calories"]
+
+
+        # Sheety - Post to Google Spreadsheet
+
+        sheety_uri = "https://api.sheety.co/2c94b4aabd362e3c421b3193d1f1a5dc/myWorkouts/workouts"
+
+        sheety_headers = {
+            "Authorization": f"Bearer {os.environ.get('SHEETY_BEARER_TOKEN')}"
+        }
+
+        sheety_data = {
+            "workout":{"date": datetime.datetime.today().strftime("%d/%m/%Y"),
+              "time": datetime.datetime.today().strftime('%H:%M:%S'),
+              "exercise": exercise,
+              "duration": duration,
+              "calories": calories,
+                     }
+        }
+
+        response = requests.post(sheety_uri, json=sheety_data ,headers=sheety_headers)
+        print(response.text)
